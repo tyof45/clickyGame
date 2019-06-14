fe790e70595107b55b2b1fb01d8cfab12bc633a9
Tyoff45 <bjur.parker45@gmail.com>
2019-06-13 17:14:04 -0700

    Updates

---
+++ precache-manifest.2e7109c1965d15621ec051ec6018a193.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb8bf5ae192aca86819648c4fce379c",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "deca6257ed576ae48065",
+    "url": "/clickyGame/static/css/main.205b4156.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/clickyGame/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "deca6257ed576ae48065",
+    "url": "/clickyGame/static/js/main.65974094.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  }
+]);
